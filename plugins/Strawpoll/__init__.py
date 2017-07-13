ace54d7f8e269dd0949017da47f4579c969969d0
Butt4cak3 <butt4cak3@gmail.com>
2017-07-13 23:44:35 +0200

    Added a strawpoll command to create polls

--- plugins/Strawpoll/__init__.py
+++ plugins/Strawpoll/__init__.py
@@ -2,10 +2,14 @@ from ircbot import Plugin
 import requests
 import time
 import html
+import json
 
 class Strawpoll(Plugin):
     lastresponse = 0
     RESPONSE_INTERVAL = 20
+
+    def init(self):
+        self.register_command('strawpoll', self.cmd_strawpoll, True)
 
     def on_privmsg(self, msg):
         now = int(time.time())
@@ -25,3 +29,20 @@ class Strawpoll(Plugin):
         options = ' | '.join(options)
 
         self.get_bot().privmsg(channel, '[Strawpoll] {} -- {}'.format(title, options))
+
+    def cmd_strawpoll(self, params, channel, sender, command):
+        if len(params) < 3:
+            self.get_bot().privmsg(channel, 'To create a poll, you have to provide a title and at least two optoins.')
+            return
+
+        poll = {
+            'title': params[0],
+            'options': params[1:]
+        }
+
+        payload = json.dumps(poll)
+
+        request = requests.post('https://strawpoll.me/api/v2/polls', data=payload)
+        response = request.json()
+
+        self.get_bot().privmsg(channel, 'https://strawpoll.me/{}'.format(response['id']))
