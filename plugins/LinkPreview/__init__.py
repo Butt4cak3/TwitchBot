2eda48fb9950f4a18b375635c03d132913cad007
Marius Becker <marius.becker.8@gmail.com>
2017-12-12 20:10:07 +0100

    Created the LinkPreview plugin
    It shows the titles of links posted in chat

---
+++ plugins/LinkPreview/__init__.py
@@ -0,0 +1,73 @@
+from ircbot import Plugin
+import requests
+import time
+import html
+import json
+import re
+
+class LinkPreview(Plugin):
+    lastresponse = 0
+    RESPONSE_INTERVAL = 20
+    enabled = True
+
+    def init(self):
+        self.register_command('linkpreview', self.cmd_linkpreview)
+        self.get_config().setdefault('previewLinks', [ 'broadcaster', 'moderator' ])
+        self.get_config().setdefault('enabled', True)
+        self.enabled = self.get_config().get('enabled')
+
+    def on_privmsg(self, msg):
+        if not self.enabled:
+            return
+
+        now = int(time.time())
+        if now < self.lastresponse + self.RESPONSE_INTERVAL:
+            return
+
+        sender = msg['sender']
+        if not self.get_bot().has_permission(sender, self.get_config()['previewLinks']):
+            return
+
+        match = re.search('https?://\S+', msg['text'])
+        if match is None:
+            return
+
+        url = match.group(0)
+        title = self.get_site_title(url)
+
+        channel = msg['channel']
+        self.get_bot().privmsg(channel, '{}: {}'.format(title, url))
+        self.lastresponse = now
+
+
+    def get_site_title(self, url):
+        request = requests.get(url)
+        response = request.text
+
+        # Don't do this at home! Parsing HTML with regular expressions is bad.
+        # But I'm also lazy, so screw it...
+        match = re.search('<\s*title[^>]*>([^<]+)<\s*/\s*title\s*>', response)
+        print(match.group(0))
+
+        if match is not None:
+            return match.group(1)
+        else:
+            return None
+
+    def cmd_linkpreview(self, params, channel, sender, command):
+        if len(params) > 0:
+            mode = params[0].lower()
+        else:
+            mode = ''
+
+        if mode in [ 'yes', 'enable', 'enabled', 'on', '1', 'true', 'y' ]:
+            self.enable_response()
+        else:
+            self.disable_response()
+
+
+    def enable_response(self):
+        self.enabled = True
+
+    def disable_response(self):
+        self.enabled = False
