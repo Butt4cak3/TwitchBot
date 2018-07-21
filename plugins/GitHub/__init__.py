f57ca01bf7b3725ede2e7124e687d400bed2faba
Marius Becker <marius.becker.8@gmail.com>
2018-07-21 11:58:06 +0200

    Added a GitHub plugin

---
+++ plugins/GitHub/__init__.py
@@ -0,0 +1,98 @@
+from ircbot import Plugin
+import re
+import requests
+import time
+
+class GitHub(Plugin):
+    def init(self):
+        self.get_config().setdefault('repos', {})
+        self.get_config().setdefault('api_key', '')
+
+    def on_privmsg(self, privmsg):
+        matches = re.finditer('#(\d+)', privmsg['text'])
+        for match in matches:
+            issue_number = match.group(1)
+
+            issue = self.get_issue_data(privmsg['channel'], issue_number)
+            if issue is None:
+                return
+
+            self.get_bot().privmsg(privmsg['channel'], '{title}: {url}'.format(**issue))
+            time.sleep(1)
+
+    def get_issue_data(self, channel, issue_number):
+        type = self.get_issue_type(channel, issue_number)
+        if type is None:
+            return
+
+        repo = self.get_repo(channel)
+        if repo is None:
+            return None
+
+        query = '''
+        {{
+            repository(owner: "{repo_owner}", name: "{repo_name}") {{
+                {type}(number: {issue_number}) {{
+                    title
+                    url
+                }}
+            }}
+        }}
+        '''.format(type=type, repo_owner=repo['owner'], repo_name=repo['name'], issue_number=issue_number)
+        result = self.run_query(query)
+        if result is None or result['data'] is None or result['data']['repository'] is None or result['data']['repository'][type] is None:
+            return None
+
+        return result['data']['repository'][type]
+
+    def get_issue_type(self, channel, issue_number):
+        repo = self.get_repo(channel)
+        if repo is None:
+            return None
+
+        query = '''
+        {{
+            repository(owner: "{repo_owner}", name: "{repo_name}") {{
+                issueOrPullRequest(number: {issue_number}) {{
+                    __typename
+                }}
+            }}
+        }}
+        '''
+        result = self.run_query(query.format(repo_owner=repo['owner'], repo_name=repo['name'], issue_number=issue_number))
+        if result is None or result['data'] is None or result['data']['repository'] is None or result['data']['repository']['issueOrPullRequest'] is None:
+            return None
+
+        if result['data']['repository']['issueOrPullRequest']['__typename'] == 'Issue':
+            return 'issue'
+        else:
+            return 'pullRequest'
+
+    def run_query(self, query):
+        api_key = self.get_config().get('api_key', None)
+        if api_key is None or api_key.strip() == '':
+            return
+
+        headers = {
+            'Authorization': 'Bearer {}'.format(api_key)
+        }
+
+        request = requests.post('https://api.github.com/graphql', json={ 'query': query }, headers=headers)
+
+        if request.status_code == 200:
+            return request.json()
+        else:
+            print('GitHub API request failed with {}'.format(request.status_code))
+            return None
+
+    def get_repo(self, channel):
+        repos = self.get_config().get('repos')
+        if not channel in repos:
+            return None
+
+        name_with_owner = repos[channel]
+        info = name_with_owner.split('/')
+        return {
+            'owner': info[0],
+            'name': info[1]
+        }
