feaef8b8a554391e82163a1cf7ceda4bb0481d14
Marius Becker <marius.becker.8@gmail.com>
2018-10-11 18:41:18 +0200

    Added an unsubscribe method to Event

--- ircbot/events.py
+++ ircbot/events.py
@@ -1,12 +1,16 @@
 class Event:
-    handlers = []
+    handlers = set()
 
     def __init__(self):
         pass
 
     def subscribe(self, handler):
-        self.handlers.append(handler)
+        self.handlers.add(handler)
 
     def invoke(self, data):
         for handler in self.handlers:
             handler(data)
+
+    def unsubscribe(self, handler):
+        if handler in self.handlers:
+            self.handlers.remove(handler)
