a96774e1e3d5c9bb8426f008893c1bd51dbf6e7b
Marius Becker <marius.becker.8@gmail.com>
2018-10-20 16:11:52 +0200

    Made the sr plugin server only

--- plugins/SongRequest/__init__.py
+++ plugins/SongRequest/__init__.py
@@ -1,51 +1,65 @@
-from twitchbot import Plugin, Permission
+from twitchbot import Plugin, Permission, Event
 from collections import deque
 import subprocess
 import threading
+import socketserver
 
 
 class SongRequest(Plugin):
     def init(self):
         self.register_command("sr", self.cmd_request,
                               permissions=(Permission.Everyone,))
-        self.player = MusicThread()
 
-    def start_player(self, url):
-        self.player = MusicThread()
-        self.player.add(url)
-        self.player.start()
+        config = self.get_config()
+        config.setdefault("host", "localhost")
+        config.setdefault("port", 9999)
+
+        self.on_request = Event()
+
+        self.server = ServerThread(self)
+        self.server.start()
 
     def cmd_request(self, params, channel, sender, command):
         url = params[0]
 
-        if not self.player.running:
-            self.start_player(url)
-        else:
-            self.player.add(url)
+        self.on_request.invoke(url)
 
         name = sender.get_displayname()
         msg = "@{} Your song was queued.".format(name)
         self.get_bot().say(channel, msg)
 
 
-class MusicThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.playlist = deque()
-        self.running = False
+class ServerThread(threading.Thread):
+    def __init__(self, plugin):
+        super().__init__()
+        self.plugin = plugin
 
     def run(self):
-        self.running = True
-        while len(self.playlist) > 0:
-            url = self.playlist.popleft()
-            downloader = subprocess.Popen(("youtube-dl", "--no-playlist", "-o",
-                                           "-", url),
-                                          stdout=subprocess.PIPE)
-            player = subprocess.Popen(("mpv", "--no-video", "-"),
-                                      stdin=downloader.stdout)
-            downloader.wait()
-            player.wait()
-        self.running = False
+        self.server = MusicServer(self.plugin)
+        self.server.serve_forever()
 
-    def add(self, url):
-        self.playlist.append(url)
+
+class MusicServer(socketserver.TCPServer):
+    def __init__(self, plugin):
+        self.plugin = plugin
+        host = plugin.get_config().get("host")
+        port = plugin.get_config().get("port")
+        super().__init__((host, port), MusicConnectionHandler)
+
+
+class MusicConnectionHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.server.plugin.on_request.subscribe(self.on_request)
+        while True:
+            data = self.request.recv(10)
+            if not data:
+                break
+
+            message = data.decode("utf-8")
+            if message == "quit":
+                break
+        self.server.plugin.on_request.unsubscribe(self.on_request)
+
+    def on_request(self, url):
+        message = url + "\n"
+        self.request.sendall(message.encode("utf-8"))
